ac95e44f7015a4ef17d7c0e0ce8165c2a8073191
wang <wang@wang-pc>
2015-11-15 14:47:13 +0800

    修改treeNode页面

--- simple-parent/simple-service/src/main/java/com/palace/seeds/endpoint/BaseEndpoint.java
+++ simple-parent/simple-service/src/main/java/com/palace/seeds/endpoint/BaseEndpoint.java
@@ -10,21 +10,21 @@ import com.palace.seeds.service.IBaseService;
 
 public abstract class BaseEndpoint {
 	
-	@RequestMapping("add")
+	 
 	public @ResponseBody Result add(Map<String,Object> params){
 		 return null;
 	}
-	@RequestMapping("edit")
+	 
 	public @ResponseBody Result edit(Map<String,Object> params){
 		return null;
 	}
 	
-	@RequestMapping("getPage")
+	 
 	public @ResponseBody Result getPageResult(Map<String,Object> params){
 		return null;
 	}
 	
-	@RequestMapping("getPage")
+	 
 	public @ResponseBody Map<String,Object> getPage(Map<String,Object> params){
 		return null;
 	}
